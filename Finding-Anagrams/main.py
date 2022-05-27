2507f5b0314fa13095fd1f34a328ec18e27afb48
TheresaNde <31648818+TheresaNde@users.noreply.github.com>
2022-05-27 23:54:37 +0100

    Update main.py

--- Finding-Anagrams/main.py
+++ Finding-Anagrams/main.py
@@ -9,8 +9,9 @@ def find_anagram(word, anagram):
 
     #word= input("Enter first word here: ")
     #anagram= input("Enter first word here: ")
-    if sorted(word) == sorted(anagram): #and len(word1)==len(word2)
-      print("This is an anagram")
+    if sorted(word) == sorted(anagram):
+       print("This is an anagram")
+       return True
       
     else: print("This is not an anagram")
 word= input("Enter first word here: ")
